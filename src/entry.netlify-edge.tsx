55d4c943ea895b0946408777fa6d45d54d2bd00d
Alexandre Bouça <alexandre.bouca.99@gmail.com>
2023-01-19 17:11:06 +0000

    Revert "dynamic import"
    This reverts commit 2da1c7739d436b0d5e444debe1d4c49cf9074cfa.

--- src/entry.netlify-edge.tsx
+++ src/entry.netlify-edge.tsx
@@ -7,13 +7,8 @@
  * - https://qwik.builder.io/integrations/deployments/netlify-edge/
  *
  */
-//import { createQwikCity } from '@builder.io/qwik-city/middleware/netlify-edge';
-//import qwikCityPlan from '@qwik-city-plan'
-import render from './entry.ssr'
+import { createQwikCity } from '@builder.io/qwik-city/middleware/netlify-edge';
+import qwikCityPlan from '@qwik-city-plan';
+import render from './entry.ssr';
 
-const { createQwikCity } = await import(
-  '@builder.io/qwik-city/middleware/netlify-edge'
-)
-const qwikCityPlan = await import('@qwik-city-plan')
-
-export default createQwikCity({ render, qwikCityPlan })
+export default createQwikCity({ render, qwikCityPlan });
